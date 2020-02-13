cddfd8c5eaba1567f87b865bcd888b66eb68b795
Abourass <abourassa@assetval.com>
2020-02-13 12:49:58 -0700

    :sparkles: Adding delete player method

--- src/class/Actions.js
+++ src/class/Actions.js
@@ -15,11 +15,13 @@ export const chooseAJob = (e, player) => {
 export const loadAJob = (player) => {
   document.getElementById('controls').innerHTML = `
     <button class="btn button is-success" id="jobControl">${player.job.btnText} - $${player.job.salary}</button>
+    <button class="btn button is-danger" id="deletePlayerControl">Delete Player</button>
   `;
   document.getElementById('job').innerText = player.job.title;
   document.getElementById('jobLvl').innerText = player.job.experience.level;
   document.getElementById('jobExp').innerText = player.job.experience.points;
   document.getElementById('jobControl').onclick = () => { player.update('money', 'add', player.job.salary); player.job.addExp(10);}
+  document.getElementById('deletePlayerControl').onclick = () => { localStorage.removeItem('player'); location.reload(); }
 };
 
 export default {chooseAJob, loadAJob}
