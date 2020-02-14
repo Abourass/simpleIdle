4b62473c22c7d82cc65def8d2cf499b5544858c7
Abourass <abourassa@assetval.com>
2020-02-14 09:54:39 -0700

    Logging out jobPath to figure out why it wont fucking assign

--- src/class/Actions.js
+++ src/class/Actions.js
@@ -2,7 +2,9 @@ import {searchJobs, searchPath} from './Job.js';
 
 export const chooseAJob = (e, player) => {
   const target = e.currentTarget;
-  player._jobPath = searchPath(target.dataset.jobCategory);
+  const jobPath = searchPath(target.dataset.jobCategory)
+  console.log('jobPath', jobPath);
+  player._jobPath = jobPath;
   console.log(player);
   player._jobPath._curLevel = 0;
   player._jobPath._curPosition = searchJobs(target.dataset.jobCategory, target.dataset.jobLevel, target.dataset.title);
