76f7b7a9422c45e0d1fda5a506f6490de4fe10ef
Abourass <abourassa@assetval.com>
2020-02-17 07:14:29 -0700

    :bug: Closed: #1

--- src/class/Actions.js
+++ src/class/Actions.js
@@ -1,10 +1,12 @@
 export const chooseAJob = (e, player) => {
   const target = e.currentTarget;
-  player.careers._currentPath = player.careers.searchPath(target.dataset.jobCategory);
-  player.careers._currentPath._curLevel = 0;
-  player.careers._currentPath._curPosition = player.careers.searchJobs(target.dataset.jobCategory, target.dataset.jobLevel, target.dataset.title);
-  player.careers._currentPath._exp = 0;
 
+  player.careers._currentPath = player.careers.searchPath(target.dataset.jobCategory);
+  player.careers._currentPath._curPosition = player.careers.searchJobs(target.dataset.jobCategory, target.dataset.jobLevel, target.dataset.title);
+  if (player._currentPath === 'none'){
+    player.careers._currentPath._curLevel = 0;
+    player.careers._currentPath._exp = 0;
+  }
   console.log('Player =>', player);
 
   document.getElementById('controls').innerHTML = `
